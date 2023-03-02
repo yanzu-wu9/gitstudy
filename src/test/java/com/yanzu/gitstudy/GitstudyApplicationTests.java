0d41a15dff6392f1fc1f5abde98a12630c06d1c0
GLM <2712861109@qq.com>
2023-03-02 18:00:44 +0800

    第一次提交

---
+++ src/test/java/com/yanzu/gitstudy/GitstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanzu.gitstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitstudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
